b7072f3c85ebe3e67993651f3fd8eb06832c68d0
root <root@localhost.localdomain>
2015-12-16 17:06:39 +0000

    First Commit

---
+++ ZHXiezi/spiders/xiezi.py
@@ -0,0 +1,30 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+import wget
+import os
+
+class ZHxiezidownloader(CrawlSpider):
+    
+    name = "xiezi"
+    allowed_domains = ["v.yupoo.com"]
+    start_urls = [
+        "http://v.yupoo.com/photos/xy0594xy/collections/"
+    ]
+  
+    rules = [Rule(SgmlLinkExtractor(restrict_xpaths=('//fieldset//div[@class="Sets"]//a')),
+                  callback='parse_item')]
+    
+    def parse_item(self, response):
+
+        mainpic = response.xpath ("//td[@valign='top']/a/img/@src").extract()[0] ## foto principal
+        pics = response.xpath ("//li/div/a/img/@src").extract() ## lista de fotos
+        folder = response.url.split("/")[6] ## ultimos parte de la url (los numeros)
+        
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        
+        wget.download(mainpic,out=folder) 
+        
+        for pic in pics:
+            wget.download(pic,out=folder)
+           
