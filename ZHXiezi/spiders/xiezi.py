d1db44059da474eadcefeca4f889f9574919e8ae
root <root@localhost.localdomain>
2015-12-21 10:38:46 +0000

    cambios flake8

--- ZHXiezi/spiders/xiezi.py
+++ ZHXiezi/spiders/xiezi.py
@@ -3,28 +3,31 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 import wget
 import os
 
+
 class ZHxiezidownloader(CrawlSpider):
-    
+
     name = "xiezi"
     allowed_domains = ["v.yupoo.com"]
     start_urls = [
         "http://v.yupoo.com/photos/xy0594xy/collections/"
     ]
-  
-    rules = [Rule(SgmlLinkExtractor(restrict_xpaths=('//fieldset//div[@class="Sets"]//a')),
-                  callback='parse_item')]
-    
+
+    rules = [Rule(SgmlLinkExtractor(restrict_xpaths=('//fieldset\
+        //div[@class="Sets"]//a')), callback='parse_item')]
+
     def parse_item(self, response):
 
-        mainpic = response.xpath ("//td[@valign='top']/a/img/@src").extract()[0] ## foto principal
-        pics = response.xpath ("//li/div/a/img/@src").extract() ## lista de fotos
-        folder = response.url.split("/")[6] ## ultimos parte de la url (los numeros)
-        
+        mainpic = response.xpath("//td[@valign='top']\
+        /a/img/@src").extract()[0]  # foto principal
+
+        pics = response.xpath("//li/div/a/img/@src").extract()  # lista de fotos
+
+        folder = response.url.split("/")[6]  # ultima parte de la url
+
         if not os.path.exists(folder):
             os.makedirs(folder)
-        
-        wget.download(mainpic,out=folder) 
-        
+
+        wget.download(mainpic, out=folder)
+
         for pic in pics:
-            wget.download(pic,out=folder)
-           
+            wget.download(pic, out=folder)
